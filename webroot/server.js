28211092a6d28c28fd92470f4d7915f09be55629
teedaa <pengtedachengllhs@gmail.com>
2025-10-10 20:27:59 -0700

    Create question and answer variables

---
+++ webroot/server.js
@@ -0,0 +1,13 @@
+const questions = [
+    "What month are we in?",
+    "What is the name of the main character in Dragon Ball Z?",
+    "How many days are in a week?",
+    "Is mayonnaise an instrument?"
+]
+
+const answers = [
+    "October",
+    "Goku",
+    "7",
+    "No"
+]
